18225e8fb5b7dbf9b50b76177e8c090422365167
PlanetZyklon <drs2001@tamu.edu>
2022-01-29 23:15:52 -0600

    Plant structs

---
+++ Assets/Scripts/PlantStructs.cs
@@ -0,0 +1,20 @@
+public struct plantStats {
+    double water = 0;
+    double health = 0;
+    double maturityTimer = 0;
+    double nutrition = 0;
+
+    // public plantStats {}
+}
+
+public struct plantGenes {
+    double maxHealth;
+    double healingRate;
+    double maturityTimer;
+    double waterCap;
+    double nutritionCap;
+    double nutritionRate;
+    double waterRate;
+
+    // public plantGenes {}
+}
